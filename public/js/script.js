5e1d883ea00273f6f1ffb49dfb4b77aacedc6375
Cauê Santos <caah.sfc@live.com>
2020-04-08 18:25:57 -0300

    Alterações na visualização do menu lateral

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
+        $('#sidebar').toggleClass('active'); 
+        let sidebar = document.getElementById('sidebar');
+        //Se o menu estiver "oculto"
+       if(sidebar.style.marginLeft === '-250px'){
+           sidebar.style.marginLeft = '0px';
+        } else {
+            sidebar.style.marginLeft = '-250px';
+        }   
     });
 });
+
+
+
